9d8df1eb000b5d0c4522ccb96f65061a6eaf9c2c
DonquixoteSabo <mixboksu@gmail.com>
2022-10-23 16:25:10 +0200

    chore: Add navbar item component

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBarItem from './NavBarItem.vue';
+</script>
+
+<template>
+  <ul class="nav">
+    <nav-bar-item to="/"> Home </nav-bar-item>
+    <nav-bar-item to="/marketing-plan"> Marketing </nav-bar-item>
+    <nav-bar-item to="/roadmap"> Roadmap </nav-bar-item>
+  </ul>
+</template>
---
+++ src/components/NavBar/NavBarItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  to: String,
+});
+</script>
+
+<template>
+  <li class="text-lg text-red-200">
+    <router-link :to="`${to}`">
+      <slot />
+    </router-link>
+  </li>
+</template>
