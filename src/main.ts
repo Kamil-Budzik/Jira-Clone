d075a681ca4041db1bc286f3f8b21e11025dc9db
DonquixoteSabo <mixboksu@gmail.com>
2022-10-23 16:53:52 +0200

    revert: Delete vue icons

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
 import { createApp } from 'vue';
-import { OhVueIcon, addIcons } from 'oh-vue-icons';
 
 import App from './App.vue';
 import router from './router';
 
 import './style.css';
 
-addIcons();
-
 const app = createApp(App);
-app.component('v-icon', OhVueIcon);
 app.use(router);
 app.mount('#app');
