77ec685eae337b8a1549e33ecb0a0f6dbc1a610c
DonquixoteSabo <mixboksu@gmail.com>
2022-10-25 14:08:38 +0200

    chore: add Main Template grid

--- src/components/Base/MainTemplate.vue
+++ src/components/Base/MainTemplate.vue
@@ -3,8 +3,33 @@ import NavBar from '@components/NavBar/NavBar.vue';
 </script>
 
 <template>
-  <div class="flex">
-    <Nav-bar />
-    <slot />
+  <div class="main-template-wrapper">
+    <div>
+      <p>LOGO</p>
+    </div>
+    <div>Page title</div>
+    <div class="nav-container">
+      <Nav-bar />
+    </div>
+    <main class="main-content">
+      <slot />
+    </main>
   </div>
 </template>
+
+<style scoped>
+.main-template-wrapper {
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 300px 1fr;
+  grid-template-rows: 100px 1fr;
+}
+.nav-container {
+  grid-column: 1;
+  grid-row: 2;
+}
+.main-content {
+  grid-column: 2;
+  grid-row: 2;
+}
+</style>
